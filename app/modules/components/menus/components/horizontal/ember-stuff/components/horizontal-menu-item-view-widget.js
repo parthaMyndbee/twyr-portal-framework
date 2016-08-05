98c1344369e9cff6c78c1ecbf3ec4f02c0c03a78
Vish Desai <vish@myndbee.com>
2016-08-05 16:47:41 +0530

    A few minor optimizations

--- app/modules/components/menus/components/horizontal/ember-stuff/components/horizontal-menu-item-view-widget.js
+++ app/modules/components/menus/components/horizontal/ember-stuff/components/horizontal-menu-item-view-widget.js
@@ -5,9 +5,7 @@ define(
 		if(window.developmentMode) console.log('DEFINE: twyr-webapp/components/horizontal-menu-item-view-widget');
 		var HorizontalMenuItemViewerWidget = _baseWidget['default'].extend({
 			'tagName': 'li',
-
 			'classNames': ['dropdown'],
-			'classNameBindings': ['model.isExpanded:open'],
 
 			'attributeBindings': ['style'],
 			'style': _ember['default'].String.htmlSafe('border-bottom:1px solid #eee; padding:5px 0px;'),
@@ -16,63 +14,12 @@ define(
 			'isVertical': false,
 			'caretClass': 'fa fa-caret-down',
 
-			'_currentlyExpandedChild': undefined,
-
 			'didInsertElement': function() {
 				var self = this;
 				self._super(...arguments);
 
 				self.set('isHorizontal', (self.get('orientation') == 'horizontal'));
 				self.set('isVertical', (self.get('orientation') == 'vertical'));
-				self._setCaretClass();
-			},
-
-			'click': function(event) {
-				event.preventDefault();
-				event.stopPropagation();
-
-				this.get('model').set('isExpanded', !this.get('model').get('isExpanded'));
-			},
-
-			'expandedChanged': _ember['default'].observer('model.isExpanded', function() {
-				this._setCaretClass();
-			}),
-
-			'onChildExpandedChanged': _ember['default'].observer('model.children.@each.isExpanded', function() {
-				var self = this,
-					expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-
-				if(expandedChild.length <= 1) {
-					self.set('_currentlyExpandedChild', undefined);
-					self.set('_currentlyExpandedChild', expandedChild[0]);
-					return;
-				}
-
-				if(self.get('_currentlyExpandedChild')) {
-					self.get('_currentlyExpandedChild').set('isExpanded', false);
-					self.set('_currentlyExpandedChild', undefined);
-				}
-
-				expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-				self.set('_currentlyExpandedChild', expandedChild[0]);
-			}),
-
-			'_setCaretClass': function() {
-				var self = this;
-
-				if(self.get('orientation') == 'vertical') {
-					if(self.get('model').get('isExpanded'))
-						self.set('caretClass', 'fa fa-caret-up');
-					else
-						self.set('caretClass', 'fa fa-caret-down');
-				}
-
-				if(self.get('orientation') == 'horizontal') {
-					if(self.get('model').get('isExpanded'))
-						self.set('caretClass', 'fa fa-caret-left');
-					else
-						self.set('caretClass', 'fa fa-caret-right');
-				}
 			}
 		});
 
@@ -89,7 +36,6 @@ define(
 			'tagName': 'li',
 
 			'classNames': ['dropdown'],
-			'classNameBindings': ['model.isExpanded:open'],
 
 			'attributeBindings': ['style'],
 			'style': _ember['default'].String.htmlSafe('border-bottom:1px solid #eee; padding:5px 0px;'),
@@ -98,60 +44,12 @@ define(
 			'isVertical': false,
 			'caretClass': 'fa fa-caret-down',
 
-			'_currentlyExpandedChild': undefined,
-
 			'didInsertElement': function() {
 				var self = this;
 				self._super(...arguments);
-				self._setCaretClass();
-			},
 
-			'click': function(event) {
-				event.stopPropagation();
-				event.preventDefault();
-
-				this.get('model').set('isExpanded', !this.get('model').get('isExpanded'));
-			},
-
-			'expandedChanged': _ember['default'].observer('model.isExpanded', function() {
-				this._setCaretClass();
-			}),
-
-			'onChildExpandedChanged': _ember['default'].observer('model.children.@each.isExpanded', function() {
-				var self = this,
-					expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-
-				if(expandedChild.length <= 1) {
-					self.set('_currentlyExpandedChild', undefined);
-					self.set('_currentlyExpandedChild', expandedChild[0]);
-					return;
-				}
-
-				if(self.get('_currentlyExpandedChild')) {
-					self.get('_currentlyExpandedChild').set('isExpanded', false);
-					self.set('_currentlyExpandedChild', undefined);
-				}
-
-				expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-				self.set('_currentlyExpandedChild', expandedChild[0]);
-			}),
-
-			'_setCaretClass': function() {
-				var self = this;
-
-				if(self.get('orientation') == 'vertical') {
-					if(self.get('model').get('isExpanded'))
-						self.set('caretClass', 'fa fa-caret-up');
-					else
-						self.set('caretClass', 'fa fa-caret-down');
-				}
-
-				if(self.get('orientation') == 'horizontal') {
-					if(self.get('model').get('isExpanded'))
-						self.set('caretClass', 'fa fa-caret-left');
-					else
-						self.set('caretClass', 'fa fa-caret-right');
-				}
+				self.set('isHorizontal', (self.get('orientation') == 'horizontal'));
+				self.set('isVertical', (self.get('orientation') == 'vertical'));
 			}
 		});
 
--- app/modules/components/menus/components/vertical/ember-stuff/components/vertical-menu-item-view-widget.js
+++ app/modules/components/menus/components/vertical/ember-stuff/components/vertical-menu-item-view-widget.js
@@ -5,59 +5,11 @@ define(
 		if(window.developmentMode) console.log('DEFINE: twyr-webapp/components/vertical-menu-item-view-widget');
 		var VerticalMenuItemViewerWidget = _baseWidget['default'].extend({
 			'tagName': 'li',
-
 			'classNames': ['dropdown'],
-			'classNameBindings': ['model.isExpanded:open'],
 
 			'attributeBindings': ['style'],
 			'style': 'border-bottom:1px solid #eee; padding:5px 0px;',
-			'caretClass': 'fa fa-caret-right',
-
-			'_currentlyExpandedChild': undefined,
-
-			'didInsertElement': function() {
-				this._super(...arguments);
-				this._setCaretClass();
-			},
-
-			'click': function(event) {
-				event.preventDefault();
-				event.stopPropagation();
-
-				this.get('model').set('isExpanded', !this.get('model').get('isExpanded'));
-			},
-
-			'expandedChanged': _ember['default'].observer('model.isExpanded', function() {
-				this._setCaretClass();
-			}),
-
-			'onChildExpandedChanged': _ember['default'].observer('model.children.@each.isExpanded', function() {
-				var self = this,
-					expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-
-				if(expandedChild.length <= 1) {
-					self.set('_currentlyExpandedChild', undefined);
-					self.set('_currentlyExpandedChild', expandedChild[0]);
-					return;
-				}
-
-				if(self.get('_currentlyExpandedChild')) {
-					self.get('_currentlyExpandedChild').set('isExpanded', false);
-					self.set('_currentlyExpandedChild', undefined);
-				}
-
-				expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-				self.set('_currentlyExpandedChild', expandedChild[0]);
-			}),
-
-			'_setCaretClass': function() {
-				var self = this;
-
-				if(self.get('model').get('isExpanded'))
-					self.set('caretClass', 'fa fa-caret-left');
-				else
-					self.set('caretClass', 'fa fa-caret-right');
-			}
+			'caretClass': 'fa fa-caret-right'
 		});
 
 		exports['default'] = VerticalMenuItemViewerWidget;
@@ -71,59 +23,11 @@ define(
 		if(window.developmentMode) console.log('DEFINE: twyr-webapp/components/vertical-component-menu-item-view-widget');
 		var VerticalComponentMenuItemViewerWidget = _baseWidget['default'].extend({
 			'tagName': 'li',
-
 			'classNames': ['dropdown'],
-			'classNameBindings': ['model.isExpanded:open'],
 
 			'attributeBindings': ['style'],
 			'style': 'border-bottom:1px solid #eee; padding:5px 0px;',
-			'caretClass': 'fa fa-caret-right',
-
-			'_currentlyExpandedChild': undefined,
-
-			'didInsertElement': function() {
-				this._super(...arguments);
-				this._setCaretClass();
-			},
-
-			'click': function(event) {
-				event.stopPropagation();
-				event.preventDefault();
-
-				this.get('model').set('isExpanded', !this.get('model').get('isExpanded'));
-			},
-
-			'expandedChanged': _ember['default'].observer('model.isExpanded', function() {
-				this._setCaretClass();
-			}),
-
-			'onChildExpandedChanged': _ember['default'].observer('model.children.@each.isExpanded', function() {
-				var self = this,
-					expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-
-				if(expandedChild.length <= 1) {
-					self.set('_currentlyExpandedChild', undefined);
-					self.set('_currentlyExpandedChild', expandedChild[0]);
-					return;
-				}
-
-				if(self.get('_currentlyExpandedChild')) {
-					self.get('_currentlyExpandedChild').set('isExpanded', false);
-					self.set('_currentlyExpandedChild', undefined);
-				}
-
-				expandedChild = self.get('model').get('sortedMenuItems').filterBy('isExpanded', true);
-				self.set('_currentlyExpandedChild', expandedChild[0]);
-			}),
-
-			'_setCaretClass': function() {
-				var self = this;
-
-				if(self.get('model').get('isExpanded'))
-					self.set('caretClass', 'fa fa-caret-left');
-				else
-					self.set('caretClass', 'fa fa-caret-right');
-			}
+			'caretClass': 'fa fa-caret-right'
 		});
 
 		exports['default'] = VerticalComponentMenuItemViewerWidget;
